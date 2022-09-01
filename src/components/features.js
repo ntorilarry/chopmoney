66f23c0a8e521fc08fe3a85c235f452eae07b729
unknown <ntorilarry@gmail.com>
2022-09-01 22:23:59 +0000

    dev-larry

--- src/components/features.js
+++ src/components/features.js
@@ -5,11 +5,11 @@ function Features() {
     <div id="what">
       <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
         <div className="flex flex-col lg:flex-row">
-          <div className="max-w-xl pr-16 mx-auto mb-10">
+          <div className="max-w-xl mx-auto mb-10">
             <img src="https://i.postimg.cc/hGsWCgFV/home.gif" alt="" />
           </div>
           <div className="grid gap-5 row-gap-5 sm:grid-cols-2">
-            <div className="max-w-xs border p-8 h-[15rem]">
+            <div className="max-w-xs border p-8 ">
               <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
                 <svg
                   className="w-12 h-12 text-deep-purple-accent-400"
@@ -25,13 +25,13 @@ function Features() {
                   />
                 </svg>
               </div>
-              <h6 className="mb-2 font-semibold leading-5">Easy-to-use</h6>
-              <p className="text-sm text-gray-700">
+              <h6 className="mb-2 text-xl font-semibold leading-5">Easy-to-use</h6>
+              <p className="text-base text-gray-700">
                 Chopmoney's interface is straight forward as we aim to make your
                 experience as snappy as possible.
               </p>
             </div>
-            <div className="max-w-xs border p-8 h-[15rem]">
+            <div className="max-w-xs border p-8">
               <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
                 <svg
                   className="w-12 h-12 text-deep-purple-accent-400"
@@ -47,14 +47,14 @@ function Features() {
                   />
                 </svg>
               </div>
-              <h6 className="mb-2 font-semibold leading-5">
+              <h6 className="mb-2 text-xl font-semibold leading-5">
                 Just like clock-work{" "}
               </h6>
-              <p className="text-sm text-gray-700">
+              <p className="text-base text-gray-700">
                 Your payments come right on your chosen time with no delay.
               </p>
             </div>
-            <div className="max-w-xs border p-8 h-[15rem]">
+            <div className="max-w-xs border p-8">
               <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
                 <svg
                   className="w-12 h-12 text-deep-purple-accent-400"
@@ -70,14 +70,14 @@ function Features() {
                   />
                 </svg>
               </div>
-              <h6 className="mb-2 font-semibold leading-5">Customised plans</h6>
-              <p className="text-sm text-gray-700">
+              <h6 className="mb-2 font-semibold text-xl leading-5">Customised plans</h6>
+              <p className="text-base text-gray-700">
                 We've all got different needs. Chopmoney knows this, so it
                 allows you to fully customise every major part of the
                 experience.
               </p>
             </div>
-            <div className="max-w-xs border p-8 h-[15rem]">
+            <div className="max-w-xs border p-8">
               <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
                 <svg
                   className="w-12 h-12 text-deep-purple-accent-400"
@@ -93,10 +93,10 @@ function Features() {
                   />
                 </svg>
               </div>
-              <h6 className="mb-2 font-semibold leading-5">
+              <h6 className="mb-2 text-xl font-semibold leading-5">
                 Totally freeeee!{" "}
               </h6>
-              <p className="text-sm text-gray-700">
+              <p className="text-base text-gray-700">
                 The Chopmoney mobile app is available on both Android and iOS
                 and comes at no charge.
               </p>
